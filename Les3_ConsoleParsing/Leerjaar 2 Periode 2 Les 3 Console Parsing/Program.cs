420117a58451709762dfa37577acb0a7f7049085
Jesse <jwissink@gmail.com>
2021-11-19 15:55:33 +0100

    Applied open/closed principle

--- Les3_ConsoleParsing/Leerjaar 2 Periode 2 Les 3 Console Parsing/Program.cs
+++ Les3_ConsoleParsing/Leerjaar 2 Periode 2 Les 3 Console Parsing/Program.cs
@@ -7,6 +7,27 @@ namespace Leerjaar_2_Periode_2_Les_3_Console_Parsing
         static void Main(string[] args)
         {
             Story story = new Story();
+
+            Room corridor = new Room("Corridor", "You're in a shiny corridor, the atmosphere is daft and you forgot why you even came here");
+            Room library = new Room("Library", "You enter the library, the fire place is lit, giving off a comfortable hue");
+            Room kitchen = new Room("Kitchen", "You enter the kitchen, there is food on the counter, but it looks like it has been there for ages");
+            Room upstairs = new Room("Hallway", "You ascent the stairs, the rug is old and daft, still there is no sign of life");
+            Room bedRoom = new Room("Bedroom", "You enter the bedroom, clearly time for a little nap");
+            Room sleep = new Room("Sleep", "You fall asleep, and you never wake again.... TUMTUMTUUUUM");
+
+
+            corridor.AddDestination(library);
+            library.AddDestination(corridor);
+            corridor.AddDestination(kitchen);
+            kitchen.AddDestination(corridor);
+            corridor.AddDestination(upstairs);
+            bedRoom.AddDestination(upstairs);
+            upstairs.AddDestination(bedRoom);
+            bedRoom.AddDestination(sleep);
+            upstairs.AddDestination(corridor);
+
+            story.startRoom = corridor;
+
             while (true)
             {
                 story.Run();
--- Les3_ConsoleParsing/Leerjaar 2 Periode 2 Les 3 Console Parsing/Story.cs
+++ Les3_ConsoleParsing/Leerjaar 2 Periode 2 Les 3 Console Parsing/Story.cs
@@ -8,36 +8,19 @@ namespace Leerjaar_2_Periode_2_Les_3_Console_Parsing
 {
     class Story
     {
-        private Room startRoom;
+        public Room startRoom { get; set; }
         private Room currentRoom;
-        private List<Room> rooms = new List<Room>();
         public Story()
         {
-            Room corridor   = new Room("Corridor", "You're in a shiny corridor, the atmosphere is daft and you forgot why you even came here");
-            Room library    = new Room("Library", "You enter the library, the fire place is lit, giving off a comfortable hue");
-            Room kitchen    = new Room("Kitchen", "You enter the kitchen, there is food on the counter, but it looks like it has been there for ages");
-            Room upstairs   = new Room("Hallway", "You ascent the stairs, the rug is old and daft, still there is no sign of life");
-            Room bedRoom    = new Room("Bedroom", "You enter the bedroom, clearly time for a little nap");
-            Room sleep      = new Room("Sleep", "You fall asleep, and you never wake again.... TUMTUMTUUUUM");
-
-
-            corridor.AddDestination(library);
-            library.AddDestination(corridor);
-            corridor.AddDestination(kitchen);
-            kitchen.AddDestination(corridor);
-            corridor.AddDestination(upstairs);
-            bedRoom.AddDestination(upstairs);
-            upstairs.AddDestination(bedRoom);
-            bedRoom.AddDestination(sleep);
-            upstairs.AddDestination(corridor);
-
-            startRoom = corridor;
-            currentRoom = startRoom;
-
-
+            
         }
+
         public void Run()
         {
+            if (currentRoom == null)
+            {
+                currentRoom = startRoom;
+            }
             Console.WriteLine($"You're in the {currentRoom.name}\n");
             Console.WriteLine(currentRoom.story + "\n");
             Console.WriteLine("Where do you want to go?");
